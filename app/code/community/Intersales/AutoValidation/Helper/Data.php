90124e45eb0a5934567b9196886fdebb8d4e7337
Daniel Rose <dr@intersales.de>
2014-02-11 14:17:26 +0100

    rename module, change code pool

---
+++ app/code/community/Intersales/AutoValidation/Helper/Data.php
@@ -0,0 +1,10 @@
+<?php
+	/**
+	 * Default Helper
+	 *
+	 * @category   Intersales
+	 * @package    Intersales_AutoValidation
+	 * @author     Daniel Rose <dr@intersales>
+	 */
+	class Intersales_AutoValidation_Helper_Data extends Mage_Core_Helper_Abstract { }
+?>
--- app/code/local/Intersales/ImmediateValidation/Helper/Data.php
+++
@@ -1,6 +0,0 @@
-<?php
-	/**
-	* 
-	*/
-	class Intersales_ImmediateValidation_Helper_Data extends Mage_Core_Helper_Abstract { }
-?>
